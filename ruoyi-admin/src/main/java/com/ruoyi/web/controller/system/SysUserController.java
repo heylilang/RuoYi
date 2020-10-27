e255c722367da9aac9648a76dfe47db25a65ff94
lilang <heylilang@hotmail.com>
2020-10-27 13:57:02 +0800

    二次提交

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
@@ -42,6 +42,7 @@ public class SysUserController extends BaseController
     private String prefix = "system/user";
 
     String aa = "dfa";
+    int a = 123;
 
     @Autowired
     private ISysUserService userService;
